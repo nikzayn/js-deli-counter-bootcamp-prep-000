5498191f0d54b12b5d583d966c078a01468016ad
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-10 09:14:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }else{
-    return 
+    return "The line is currently empty.";
   }
+  return position;
 }
